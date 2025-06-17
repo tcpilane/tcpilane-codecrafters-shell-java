6519d0bbea699fb65f51cca84c0df0d213d133b2
Tshegofatso Pilane <tspilanjhb024@student.wethinkcode.co.za>
2025-06-17 12:22:45 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,14 @@ public class Main {
         while (true) {
             Scanner scanner = new Scanner(System.in);
             input = scanner.nextLine();
+
+            if (input.equals("exit 0")) {
+                System.exit(0);
+            }
             System.out.println(input + ": command not found");
             System.out.print("$ ");
+
+
         }
     }
 }
