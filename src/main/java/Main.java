4b95da3f82a3bf11260fe534698a4b5191ef78bf
Tshegofatso Pilane <tspilanjhb024@student.wethinkcode.co.za>
2025-06-17 08:53:34 +0200

    Handle invalid commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("$ ");
+        System.out.print("$ ");
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         System.out.println(input + ": command not found");
